9a6122899aa8eb28c061015799e018d0b746470d
Creator William <creator_william@icloud.com>
2018-10-30 16:51:19 +0800

    ADD 增加占位图类型，方便创建占位图

---
+++ ComponentKit/Placeholder/Placeholder.swift
@@ -0,0 +1,17 @@
+//
+//  Placeholder.swift
+//  ComponentKit
+//
+//  Created by William Lee on 2018/10/30.
+//  Copyright © 2018 William Lee. All rights reserved.
+//
+
+import UIKit
+
+public struct Placeholder {
+  
+  public let name: String
+  public var image: UIImage? { return UIImage(named: self.name) }
+  
+  public init(_ name: String) { self.name = name }
+}
